91dd455e4fdac0893dee9f2de7d3d5bf7a73686d
Kate Gaeva <ekaterinagaeva004@gmail.com>
2023-06-05 16:16:31 +0500

    week9

---
+++ week9/index.js
@@ -0,0 +1,30 @@
+let NightCleanerSalary = 40000;
+let VocalTeacher = 22000;
+console.log(`${NightCleanerSalary + VocalTeacher}` + ' ' + 'может зарабатывать вечерний уборщик, имея голос и педагогическое образование');
+
+console.log('Владивосток' + ' ' + 2000);
+
+console.log(`${(128 + 254 + 137 + 201) / 60}` + ' ' + 'часов прокрастинировала Русалка');
+
+console.log(2 ** 5);
+
+console.log(`${"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"}`);
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ', ' + name);
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву!');
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
+
