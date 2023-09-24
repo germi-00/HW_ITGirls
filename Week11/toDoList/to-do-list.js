025cd750801e7eef2ca32620c73cea833d4c7a7d
Kate Gaeva <ekaterinagaeva004@gmail.com>
2023-09-24 22:22:59 +0500

    toDoList

---
+++ Week11/toDoList/to-do-list.js
@@ -0,0 +1,20 @@
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const ul = document.getElementById("result");
+
+function getResult() {
+    let result = input.value;
+    let li = document.createElement("li");
+    li.textContent = result;
+    ul.append(li);
+    input.value = '';
+}
+
+button.addEventListener('click', getResult);
+
+const list = document.querySelector('ul');
+list.addEventListener('click', function (evt) {
+    if (evt.target.tagName === 'LI') {
+        evt.target.classList.toggle('task-is-done');
+    }
+});
